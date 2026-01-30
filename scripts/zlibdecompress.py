672d97bbb1c589301d95089a191c9a8748e23103
BrianWilkinsFL <106542601+BrianWilkinsFL@users.noreply.github.com>
2026-01-30 16:35:39 -0500

    Create zlibdecompress.py

---
+++ scripts/zlibdecompress.py
@@ -0,0 +1,10 @@
+# This file will do test decompressions of the zlib compressed bitmaps for HamClock
+# Replace input and output filenames below
+# It is a throwaway script
+import zlib, sys
+
+with open("map-D-660x330-Clouds.bmp.z", "rb") as f:
+   data = zlib.decompress(f.read())
+
+with open("output.bmp", "wb") as f:
+   f.write(data)
