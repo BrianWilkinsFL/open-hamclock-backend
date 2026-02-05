b6a3d81822409428e4b475cb7f26b1817db49209
BrianWilkinsFL <106542601+BrianWilkinsFL@users.noreply.github.com>
2026-02-04 20:59:12 -0500

    Revert "Bring docker up to date with latest requirements"

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,2 +1,1 @@
-# this will be a docker-compose file.
-docker run -ti --rm --net hamclock -d --name=hamclock-be hamclock-be:test
+docker run -ti --rm --net hamclock --ip=172.19.0.3 -d --name=hamclock-be hamclock-be:test
